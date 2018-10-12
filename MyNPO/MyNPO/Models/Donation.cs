16da911ebe531f6dd4f828f51757e756e999a6dd
Senbaga Kumar Sigamani (Infosys Ltd) <v-sesiga@microsoft.com>
2018-10-12 15:55:37 -0700

    Added System donation page.

---
+++ MyNPO/MyNPO/Models/Donation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyNPO.Models
+{
+    public class Donation
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public string DonationAmount { get; set; }
+        [Required]
+        public string Reason { get; set; }
+    }
+}
